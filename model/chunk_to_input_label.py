1a72bc467a3ac1dc020bb5bcfd1dc53cae6530b0
LihiHadjb <48348862+LihiHadjb@users.noreply.github.com>
2020-10-16 18:21:53 +0300

    k

--- model/chunk_to_input_label.py
+++ model/chunk_to_input_label.py
@@ -33,14 +33,25 @@ class ChunkToInputLabel:
                 return True
         return False
 
+    def is_valid_chunk(self, chunk):
+        for c in chunk:
+            if not (self.cid.is_nikud(c) or self.cid.is_geresh(c) or self.cid.is_alphabet(c)):
+                return False
+        return True
+
     def chunk_to_input_label(self, chunk):
         letter_ch = chunk[0]
-        nikud = self.get_nikud_in_chunk(chunk)
-        nikud_idx = self.cid.get_nikud_to_idx(nikud)
+
         if self.has_geresh(chunk):
             letter_idx = self.cid.get_letter_with_geresh_to_idx(letter_ch)
         else:
             letter_idx = self.cid.get_letter_no_geresh_to_idx(letter_ch)
+
+        if not self.is_valid_chunk(chunk):
+            return letter_idx, self.cid.get_no_nikud_idx()
+
+        nikud = self.get_nikud_in_chunk(chunk)
+        nikud_idx = self.cid.get_nikud_to_idx(nikud)
         return letter_idx, nikud_idx
 
     def get_sep_chunk(self):
