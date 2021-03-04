02dd429dc075bcc16c4419f7e6713c13d4f485dc
LihiHadjb <48348862+LihiHadjb@users.noreply.github.com>
2021-03-04 18:10:55 +0200

    home page styling

--- web/myApp.py
+++ web/myApp.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request
 from web.forms import InputText
-from model.lstm.useModel import get_prediction_for_text
+from model.lstm.predict import get_prediction_for_text
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
+cnt = 0
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     input_form = InputText()
     input_form.input.data = ""
     output_text = ""
+    global cnt
     if request.method == "POST":
         input_text = request.form['input']
         input_form.input.data = input_text
-        output_text = get_prediction_for_text(input_text)
+        #output_text = get_prediction_for_text(input_text)
+        output_text = str(cnt)
+        cnt+=1
     return render_template('home.html', form=input_form, output=output_text)
 
 
