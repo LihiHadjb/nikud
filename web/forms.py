cdee68157f6d0fb3059aab2e1fc6f2519b4f02d0
LihiHadjb <48348862+LihiHadjb@users.noreply.github.com>
2021-03-04 22:38:59 +0200

    some front

--- web/forms.py
+++ web/forms.py
@@ -1,8 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
+from wtforms.validators import DataRequired, Email
 
 
 class InputText(FlaskForm):
     input = TextAreaField()
-    submit = SubmitField('נקד לי!')
+    submit = SubmitField()
+
+
+#TODO: put the label to the right of the field
+class ContactForm(FlaskForm):
+    type = SelectField(
+        'סוג הפנייה',
+        [DataRequired()],
+        choices=[
+            ('bug', 'דיווח על באג'),
+            ('suggestion', 'הצעה לשיפור'),
+            ('other', 'סתם בא לי לדבר')
+        ]
+    )
+
+    content = TextAreaField()
+    email = StringField(
+        'Email',
+        [
+            Email(message='כתובת המייל אינה תקנית'),
+        ]
+    )
+    submit = SubmitField()
 
--- web/myApp.py
+++ web/myApp.py
@@ -1,26 +1,39 @@
 from flask import Flask, render_template, request
-from web.forms import InputText
+from web.forms import InputText, ContactForm
 from model.lstm.predict import get_prediction_for_text
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
-cnt = 0
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     input_form = InputText()
     input_form.input.data = ""
     output_text = ""
-    global cnt
     if request.method == "POST":
         input_text = request.form['input']
         input_form.input.data = input_text
         #output_text = get_prediction_for_text(input_text)
-        output_text = str(cnt)
-        cnt+=1
+        output_text = "וָאָיְ וָאחִד מֻמְכִּן יִסָאוִיהָא בִּאלְבֵּית"
     return render_template('home.html', form=input_form, output=output_text)
 
 
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    return render_template('contact.html', form=form)
 
+
+@app.route("/help", methods=['GET'])
+def helpp():
+    return render_template('help.html')
+
+@app.route("/about", methods=['GET'])
+def about():
+    return render_template('about.html')
+
+@app.route("/intro", methods=['GET'])
+def intro():
+    return render_template('intro.html')
